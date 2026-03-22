6589d743543580c554736ca1ba1b37900299f3cb
dipesh shrestha <np03cs4a220112@heraldcollege.edu.np>
2026-03-22 15:09:19 +0545

    updated visuals and click features

--- frontend/script.js
+++ frontend/script.js
@@ -1,13 +1,23 @@
 async function shorten() {
     const url = document.getElementById("urlInput").value;
 
-    const res = await fetch("http://localhost:5000/shorten", {
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify({url})
-    });
+    if (!url){
+        alert("Please enter a URL");
+        return;
+    }
+
+    try{
+        const res = await fetch("http://localhost:5000/shorten", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({url})
+        });
 
     const data = await res.json();
 
-    document.getElementById("result").innerText = data.short_url;
+    document.getElementById("result").innerHTML = `<a href="${data.short_url}" target="_blank">${data.short_url}</a>`;
+    } catch (err){
+        console.error("Error:", err);
+        alert("Something went wrong. Please try again.");
+    }
 }
