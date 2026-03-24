4768ef2d254a99f2cbb7e32f619f45a75152ac9a
dipesh shrestha <np03cs4a220112@heraldcollege.edu.np>
2026-03-24 12:33:15 +0545

    fixed empty url box issue and added some final touchups

--- backend/app/db.py
+++ backend/app/db.py
@@ -3,4 +3,3 @@ from pymongo import MongoClient
 client  = MongoClient("mongodb://mongo:27017/")
 db = client["url-shortener"]
 collection = db["urls"]
-
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,6 +1,5 @@
-# backend/app/routes.py
 from flask import Blueprint, request, jsonify, redirect
-from .db import collection  # MongoDB collection
+from .db import collection  
 from functools import wraps
 import time
 import random
@@ -8,9 +7,8 @@ import string
 
 main = Blueprint("main", __name__)
 
-# -------------------
 # Rate Limiter
-# -------------------
+
 rate_limit_data = {}
 MAX_REQUESTS = 5
 TIME_WINDOW = 60  # seconds
@@ -38,15 +36,14 @@ def rate_limiter(func):
         return func(*args, **kwargs)
     return wrapper
 
-# -------------------
-# Helpers
-# -------------------
+
+# code generator
 def generate_code(length=6):
     return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
 
-# -------------------
-# API Routes
-# -------------------
+
+# api route to shorten url
+
 
 @main.route("/shorten", methods=["POST"])
 @rate_limiter
@@ -54,7 +51,7 @@ def shorten():
     data = request.json
     long_url = data.get("url")
 
-    # check duplicate
+    # check for duplicate
     existing = collection.find_one({"long_url": long_url})
     if existing:
         return jsonify({"short_url": f"http://localhost:5000/{existing['code']}"})
@@ -74,9 +71,9 @@ def shorten():
 
     return jsonify({"short_url": f"http://localhost:5000/{code}"})
 
-# -------------------
-# Analytics endpoint
-# -------------------
+
+# Analytics route to check data
+
 @main.route("/analytics", methods=["GET"])
 def analytics():
     urls = list(collection.find({}, {"_id": 0, "code": 1, "long_url": 1, "click_times": 1}))
@@ -85,9 +82,9 @@ def analytics():
             u["click_times"] = []
     return jsonify(urls)
 
-# -------------------
+
 # Redirect endpoint
-# -------------------
+
 @main.route("/<code>")
 def redirect_url(code):
     result = collection.find_one({"code": code})
